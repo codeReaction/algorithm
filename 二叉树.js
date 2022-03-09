30a7c20b7ee721409b0eba51e777d537109e7b80
Shine Lee@laptop <1945161171@qq.com>
2022-03-09 13:09:17 +0800

    auto save

--- 二叉树.js
+++ 二叉树.js
@@ -1,67 +1,65 @@
-
 // 二叉树结构
-function TreeNode(val){
+function TreeNode(val) {
   this.val = val
   this.left = this.right = null
 }
 
 // 递归序遍历
-function traversal(root){
+function traversal(root) {
   // 第一次进入该层
-  if(root == null) return
+  if (root == null) return
 
   traversal(root.left) //进入左树下层递归
   // 返回该层，第二次进入
 
   traversal(root.right) //进入右树下层递归
-  // 返回该层，第三次进入 
+  // 返回该层，第三次进入
 }
-
 
 // 先序遍历/深度遍历
 // 第一次进入时输出
-function preOrderTraversal(root){
-  function preOT(root){
-    if(root == null) return
+function preOrderTraversal(root) {
+  function preOT(root) {
+    if (root == null) return
     arr.push(root.val)
     preOT(root.left)
     preOT(root.right)
   }
-  let arr=[]
+  let arr = []
   preOT(root)
   return arr
 }
 
 // 中序遍历
 // 第二次进入时输出
-function inOrderTraversal(root){
-  function inOT(root){
-    if(root == null) return
+function inOrderTraversal(root) {
+  function inOT(root) {
+    if (root == null) return
     inOT(root.left)
     arr.push(root.val)
     inOT(root.right)
   }
-  let arr=[]
+  let arr = []
   inOT(root)
   return arr
 }
 
 // 后序遍历
 // 第三次进入时输出
-function posOrderTraversal(root){
-  function posOT(root){
-    if(root == null) return
+function posOrderTraversal(root) {
+  function posOT(root) {
+    if (root == null) return
     posOT(root.left)
     posOT(root.right)
     arr.push(root.val)
   }
-  let arr=[]
+  let arr = []
   posOT(root)
   return arr
 }
 
 // 宽度遍历
-function widthErgodic(root){
+function breadthErgodic(root) {
   /* 利用队列，先放入根节点
     每次弹出一个节点就输出
     再放入弹出节点的左右子树，先左后右
@@ -73,15 +71,17 @@ function widthErgodic(root){
 
   let arr = [] //保存结果
 
-  while(queue.length){
+  while (queue.length) {
     let cur = queue.shift() //弹出节点
     arr.push(cur.val)
 
-    if(cur.left!=null){ //压入左节点
+    if (cur.left != null) {
+      //压入左节点
       queue.push(cur.left)
     }
 
-    if(cur.right!=null){ //压入右节点
+    if (cur.right != null) {
+      //压入右节点
       queue.push(cur.right)
     }
   }
@@ -92,5 +92,5 @@ module.exports = {
   TreeNode,
   preOrderTraversal,
   inOrderTraversal,
-  posOrderTraversal
+  posOrderTraversal,
 }
